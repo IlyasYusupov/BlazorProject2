84b499ad3e8f158d63fb13bb0d8b750c8fd6050b
Ilyas Yusupov <iu70980@gmail.com>
2022-10-29 00:06:08 +0300

    1

---
+++ BlazorControlwork/Data/FileSystemService.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Driver.GridFS;
+using MongoDB.Driver;
+
+namespace BlazorControlwork.Data
+{
+    public class FileSystemService
+    {
+        public void UploadImageToDb()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("UserBase");
+            var collection = database.GetCollection<string>("Images");
+            var gridFS = new GridFSBucket(database);
+
+            using (FileStream fs = new FileStream(@"C:\Users\iu709\OneDrive\Рабочий стол\1.png", FileMode.Open))
+            {
+                gridFS.UploadFromStream("sss.png", fs);
+            }
+        }
+
+        public void DownloadToLocal()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Images321");
+            var gridFS = new GridFSBucket(database);
+            using (FileStream fs = new FileStream($"{Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/Images/")}DeserializedBall.png", FileMode.CreateNew))
+            {
+                gridFS.DownloadToStreamByName("sss.jpg", fs);
+            }
+        }
+    }
+}
